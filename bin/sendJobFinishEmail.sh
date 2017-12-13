584737e938a0228bb0b712f9096444bf89a0cf65
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2017-12-13 09:33:27 -0500

    csend better email

--- bin/sendJobFinishEmail.sh
+++ bin/sendJobFinishEmail.sh
@@ -8,13 +8,16 @@ minimumsize=9000
 
 actualsize=`wc -c $flag.out`
 
-[ -f $flag.failed ] && s="Subject: Failed: id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" ||  s="Subject: Success: id:$SLURM_JOBID name:$SLURM_JOB_NAME\n"
+[ -f $flag.failed ] && s="Subject: Failed: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" ||  s="Subject: Success: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n"
+
+stat=`tail -n 1 $flag.out`
+[[ "$stat" == *COMPLETED* ]] && echo Notice the sacct report above: while the main job is still running for sacct command, user task is completed. >> $flag.out
 
 if [ "${actualsize% *}" -ge "$minimumsize" ]; then
-   toSend=`cat $flag.sh`
+   toSend=`echo Job script content:; cat $flag.sh`
    toSend="$s\n$toSend\nOutput is too big for email. Please find output in: $flag.out"  
 else
-   toSend=`cat $flag.sh $flag.out`
+   toSend=`echo Job script content:; cat $flag.sh; echo Job output:; cat $flag.out`
    toSend="$s\n$toSend"
 fi
 
