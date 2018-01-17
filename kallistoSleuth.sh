03a7339efbaacd0a755537a6f313397149502e2f
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2018-01-17 12:13:35 -0500

    add Kallisto and Sleuth

---
+++ kallistoSleuth.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+usage() { echo -e "\nUsage : `basename $0` <-i transcriptome index (required: currently we have: RCh38, GRCm38, BDGP6 and GRCz10)>\n"; exit 1;} 
+
+while getopts ":i:" o; do
+    case "${o}" in
+        i)
+            i=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+[ -z "$i" ] && usage
+
+module purge
+
+module load rcbio/1.0 gcc/6.2.0 kallisto/0.43.1  R/3.4.1 
+
+SHARED_DATABASES=/n/shared_db
+
+# set the correct index and gene annotation file
+if [ -f "${i}" ]; then
+   index=$i
+else 
+  case "$i" in
+    "GRCh38")index="$SHARED_DATABASES/GRCh38/91/kallisto/0.43.1/GRCh38"
+            s=hsapiens_gene_ensembl
+    ;;
+    
+    "GRCm38")index="$SHARED_DATABASES/GRCm38/91/kallisto/0.43.1/GRCm38"
+            s=mmusculus_gene_ensembl
+    ;;
+    
+    "BDGP6")index="$SHARED_DATABASES/BDGP6/91/kallisto/0.43.1/BDGP6"
+            s=dmelanogaster_gene_ensembl
+    ;;
+    
+    "GRCz10")index="$SHARED_DATABASES/GRCz10/91/kallisto/0.43.1/GRCz10"
+            s="drerio_gene_ensembl"
+    ;;
+  
+  
+    *)  echo "Index '$i' is not supported. Please email rchelp@hms.harvard.edu for help."; usage
+    ;;
+  esac
+fi
+
+[ -d group2/ ] || { echo group2 is not found. You need at least two groups to run this pipeline; exit 1; }
+
+echo -e "sample\tgroup\tpath" > sample.lst 
+
+mkdir -p kallistoOut
+
+#loopStart,group
+for group in `ls -d group*/|sed 's|[/]||g'`; do
+  
+    echo working on group:  $group
+    #loopStart,sample
+    for sample in `ls -d $group/*/ | xargs -n 1 basename`; do
+         
+        echo working on sample: $sample
+        fileList=`ls  $group/$sample/*{_1.fastq,_1.fq,_r1.fastq,_r1.fq,_R1.fastq,_R1.fq}* 2>/dev/null`
+        echo fileList: $fileList
+        
+        [ -z "$fileList" ] && { echo Read file not found for $group/$sample! Please make sure the fastq files are named as xxx_1.fastq, xxx_2.fastq or xxx_1.fq, xxx_2.fq or xxx_r1.fastq, xxx_r2.fastq or xxx_R1.fq, xxx_R2.fq or xxx_1.fastq.gz, xxx_2.fastq.gz or xxx_1.fq.gz, xxx_2.fq.gz or xxx_r1.fastq.gz, xxx_r2.fastq.gz or xxx_R1.fq.gz, xxx_R2.fq.gz;  exit 1; }
+        
+        reads=""
+        for r1 in `echo $fileList | xargs -n 1 basename`; do 
+            #echo working on read one file: $r1
+            
+            readgroup=${r1%_*}
+            echo working on readgroup: $readgroup
+            ext=${r1##*_}; ext=${ext/1/2}
+            r2=${readgroup}_$ext
+                                  
+            [[ -f $group/$sample/$r2 ]] && r2="$group/$sample/$r2" || { echo -e "\n\n!!!Warning: read2 file '$r2' not exist, ignore this warning if you are working with single-end data\n\n"; r2=""; }
+            reads="$reads  $group/$sample/$r1 $r2"     
+        done
+        
+        echo -e "$sample\t$group\tkallistoOut/$group$sample" >> sample.lst 
+        
+        [ -z $r2 ] && reads="-single $reads"
+        
+        #@1,0,kallisto2,,sbatch -p short --mem 32G -n 4 -t 12:0:0 
+        rm -r kallistoOut/$group$sample 2>/dev/null ; kallisto quant -o kallistoOut/$group$sample -b 100 -t 4 -i $index $reads
+          
+        
+    #loopEnd 
+    done 
+#loopEnd     
+done
+
+#@2,1,sleuth 
+export R_LIBS_USER=/home/kmh40/R-3.4.1-sleuth; Rscript sleuth.r $s
---
+++ sleuth.r
@@ -0,0 +1,39 @@
+#!/usr/bin/env Rscript
+
+args<-commandArgs(TRUE)
+
+sp=args[1]
+
+#source("http://bioconductor.org/biocLite.R")
+
+#biocLite("devtools", suppressUpdates=TRUE)
+
+#biocLite("pachterlab/sleuth", suppressUpdates=TRUE)
+
+library("sleuth")
+
+sample=read.table("sample.lst", header = TRUE, stringsAsFactors=FALSE)
+
+if (sp != "") {
+    library("biomaRt", lib.loc="/home/kmh40/R-3.4.1-cl323")    
+    mart=biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",dataset=sp, host = "useast.ensembl.org")
+    t2g=biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart = mart)
+
+    t2g=dplyr::rename(t2g, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+
+    sout=sleuth_prep(sample, ~ group, target_mapping = t2g)
+} else {
+    sout=sleuth_prep(sample, ~ group)
+}
+    
+sout=sleuth_fit(sout)
+
+sout=sleuth_wt(sout, 'groupgroup2')
+
+results=sleuth_results(sout, 'groupgroup2')
+
+results_ordered=results[order(results$qval),]
+
+write.table(results_ordered, file='sleuth.DE_transcripts.txt', sep="\t",row.names=F, quote=F)
+
+write.table( subset(results_ordered, qval <= 0.05), file='sleuth.DE_transcripts.qval_0.05.txt', sep="\t",row.names=F, quote=F)
