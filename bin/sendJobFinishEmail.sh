69237adbbf51ef9ec032c283e4108ed8fd07be64
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2019-02-06 08:17:50 -0500

    fix a bug, take more options to send email

--- bin/sendJobFinishEmail.sh
+++ bin/sendJobFinishEmail.sh
@@ -4,22 +4,29 @@ to=`cat ~/.forward`
 
 flag=$1
 
+# call by: sendJobFinishEmail fullPathFlag or sendJobFinishEmail out.txt err.txt script.txt 
+[ -z "$2" ] && { out=$flag.out; script=$flag.sh; ss=$flag.success; } || { out="$1"; err="$2"; script="$3"; }
+
 minimumsize=9000
 
-actualsize=`wc -c $flag.out`
+actualsize=`wc -c $out`
 
-[ ! -f $flag.success ] && s="Subject: Failed: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" ||  s="Subject: Success: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n"
+[ -z "$2" ] && [ ! -f $ss ] && s="Subject: Failed: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" ||  s="Subject: Success: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n"
 
-stat=`tail -n 1 $flag.out`
-[[ "$stat" == *COMPLETED* ]] && echo *Notice the sacct report above: while the main job is still running for sacct command, user task is completed. >> $flag.out
+[ ! -z "$2" ] && `grep jobSuccessfullyDone $out` &&   s="Subject: Success: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" || s="Subject: Failed: job id:$SLURM_JOBID name:$SLURM_JOB_NAME\n" 
+
+stat=`tail -n 1 $out`
+[[ "$stat" == *COMPLETED* ]] && echo *Notice the sacct report above: while the main job is still running for sacct command, user task is completed. >> $out
 
 if [ "${actualsize% *}" -ge "$minimumsize" ]; then
-   toSend=`echo Job script content:; cat $flag.sh`
-   toSend="$s\n$toSend\nOutput is too big for email. Please find output in: $flag.out"  
-   toSend="$toSend\n...\n`tail -n 6 $flag.out`"
+   toSend=`echo Job script content:; cat $script`
+   toSend="$s\n$toSend\nOutput is too big for email. Please find output in: $out"  
+   toSend="$toSend\n...\n`tail -n 6 $out`"
+   [ -f "$err" ] && toSend="$toSend\n...Err:\n`tail -n 6 $err`"
 else
-   toSend=`echo Job script content:; cat $flag.sh; echo Job output:; cat $flag.out`
+   toSend=`echo Job script content:; cat $script; echo Job output:; cat $out`
    toSend="$s\n$toSend"
+   [ -f "$err" ] && toSend="$toSend\n...Err:\n`tail -n 6 $err`"
 fi
 
 echo -e "$toSend" | sendmail $to 
