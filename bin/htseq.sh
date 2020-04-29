baf7ac44e16ca2251fb660e3e7accc14bbb5fe64
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2020-04-29 11:28:41 -0400

    add strand option for htseq-cout, remove adapter

--- bin/htseq.sh
+++ bin/htseq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage() { echo -e "Usage :\n${0##*/} <-p path_of_alignment, such as: bowtieOut, bwaOut or starOut, required> [-r species_index (required if no -g. Such as: dm3, dm6, mm10, hg18, hg19 or hg38. Let us know if you need other references)] [-g .gtf file with full path (required if no -r, don't need this if -r is given)]"; exit 1;}
+usage() { echo -e "Usage :\n${0##*/} <-p path_of_alignment, such as: bowtieOut, bwaOut or starOut, required> [-r species_index (required if no -g. Such as: dm3, dm6, mm10, hg18, hg19 or hg38. Let us know if you need other references)] [-g .gtf file with full path (required if no -r, don't need this if -r is given)] <-s strand(yes,no,reverse)>"; exit 1;}
 
 while getopts ":p:r:g:" o; do
     case "${o}" in
@@ -12,6 +12,9 @@ while getopts ":p:r:g:" o; do
             ;;
         p)  
             p=${OPTARG}
+            ;;
+        s)
+            s=${OPTARG}
             ;;
         
         *)
@@ -28,6 +31,8 @@ done
 [ -z "$p" ] && { echo Please provide the alignment folder for option -p such as -p bwaOut, -p bowtieOut or -p starOut;  usage; }
 
 [ -d $p ] || { echo Folder not exist: $p; usage; }
+
+[ -z "$s" ] && { echo Please provide the strand for option -s;  usage; }
 
 module load gcc/6.2.0  python/2.7.12  htseq/0.9.1 samtools/0.1.19
 
@@ -89,10 +94,8 @@ for group in `ls -v -d group*/|sed 's|[/]||g'`; do
         [ -f $i ] || { echo bam file not find: $i; exit 1; }
         
         #@1,0,htseq,,sbatch -p short -t 12:0:0 --mem 8G
-        samtools view -bf 1 $i > $i.pe.bam && samtools sort -n $i.pe.bam $i.sorted && htseq-count -s no -f bam $i.sorted.bam $gtf > $i.read.count.txt
-
+        samtools view -bf 1 $i > $i.pe.bam && samtools sort -n $i.pe.bam $i.sorted && htseq-count -s $s -f bam $i.sorted.bam $gtf > $i.read.count.txt
     done
-
 done
 
 module load R/3.4.1
